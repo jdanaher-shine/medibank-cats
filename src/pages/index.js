7601725edc25bd66dc0c95fe1b85316be0aeb740
Jonathan Danaher <jonathan.danaher@shinesolutions.com>
2020-05-13 22:16:22 +1000

    Fix naming bug on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import People from "../components/people";
+import Cats from "../components/cats";
 
 const IndexPage = () => (
   <Layout>
